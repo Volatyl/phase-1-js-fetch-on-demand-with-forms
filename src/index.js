b23e3ea74f19f1fe2471afd06d787ac4f25fc395
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-24 18:05:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  const form = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+
+    const input = document.querySelector("#searchByID").value;
+    console.log(input);
+
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
